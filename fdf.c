18b564aa392a00ffcecc1e47f34e7df90ef098d3
Edu <bfwaend@gmail.com>
2022-10-18 18:51:56 -0300

    Not compiling, but going places

--- fdf.c
+++ fdf.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+	/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   fdf.c                                              :+:      :+:    :+:   */
@@ -6,7 +6,7 @@
 /*   By: etachott <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 17:33:35 by etachott          #+#    #+#             */
-/*   Updated: 2022/10/18 17:34:07 by edu              ###   ########.fr       */
+/*   Updated: 2022/10/18 18:50:18 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,36 @@ int	key_press_events(int keycode, t_mlx_vars *mlx)
 	return (0);
 }
 
-int	**create_matrix_from_file(char *file)
+int	**create_matrix_from_file(char *file, size_t lines)
 {
-//	char	*line;
-//	char	**tmp_matrix;
+	char	*line;
+	char	**tmp_matrix;
 	int		**final_matrix;
 	int		fd;
+	int		i;
+	int		j;
 
+	i = 0;
+	j = 0;
 	fd = open(file, O_RDONLY);
-	final_matrix = malloc(sizeof(int *) * ft_count_lines(file));
-//	tmp_matrix = ft_split(line, ' ');
+	final_matrix = (int **)malloc(sizeof(int *) * lines + 1);
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		tmp_matrix = ft_split(line, ' ');
+		while (1)
+		{
+			ft_printf("oi\n");
+			final_matrix[i] = ft_mtoia(tmp_matrix);
+			if (!final_matrix[i])
+				break ;
+		}
+		i++;
+	}
 	close(fd);
-//	free(line);
+	free(line);
 	return (final_matrix);
 }
 
@@ -44,11 +62,13 @@ int	main(int argc, char *argv[])
 {
 	t_mlx_vars	mlx;
 	t_data		img;
-	//t_map		map;
+	t_map		map;
+	size_t		lines;
 
 	if (argc != 2)
 		return (ft_printf("Usage: ./fdf [FILE]\n"));
 	ft_printf("FILE = %s\n", argv[1]);
+	lines = ft_count_lines(argv[1]);
 	mlx.mlx = mlx_init();
 	mlx.window = mlx_new_window(mlx.mlx, 800, 600, "FdF");
 	img.img = mlx_new_image(mlx.mlx, 800, 600);
@@ -56,8 +76,8 @@ int	main(int argc, char *argv[])
 			&img.endian);
 	paint_image(img, 800, 600);
 	put_triangle(img);
-	//map.matrix = create_matrix_from_file(argv[1]);
-	//print_matrix(map.matrix);
+	map.matrix = create_matrix_from_file(argv[1], lines);
+	print_matrix(map.matrix);
 	mlx_put_image_to_window(mlx.mlx, mlx.window, img.img, 0, 0);
 	mlx_loop_hook(mlx.mlx, &handle_no_event, (void *) &mlx);
 	mlx_hook(mlx.window, 2, 1L << 0, key_press_events, &mlx);
--- fdf_utils.c
+++ fdf_utils.c
@@ -6,7 +6,7 @@
 /*   By: edu <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 17:12:03 by edu               #+#    #+#             */
-/*   Updated: 2022/10/18 17:28:45 by edu              ###   ########.fr       */
+/*   Updated: 2022/10/18 18:50:33 by edu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,4 +30,20 @@ size_t	ft_count_lines(char *file)
 	close(fd);
 	free(line);
 	return (i);
+}
+
+int	*ft_mtoia(char **matrix, size_t lines)
+{
+	int	*array;
+	int	i;
+
+	array = malloc(sizeof(int) * lines);
+	if (!array)
+		return (NULL);
+	while (matrix[i])
+	{
+		array[i] = ft_atoi(matrix[i]);
+		i++;
+	}
+	return (array);
 }
